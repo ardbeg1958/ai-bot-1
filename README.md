ce31c1e87c50b179dbb62362492541ada2b124ce
SAKO Hiroshi <sakoh@ba2.so-net.ne.jp>
2024-02-20 23:24:01 +0900

    仕様は README を参照。基本的な対話ができるようになった。

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ai-bot-1
 experimental AI bot
+
+# 仕様メモ
+
+1 ページ目
+    API キー入力（OpenAIのキーを登録）
+2 ページ目
+    インストラクション入力（編集可能）
+    対話履歴5回分保持
+    Push - to - Talk 方式。ボタンを押すと聞き取り開始で、話が途切れると（無音を検出）API を呼び出し回答を得る
+    回答が返ってきたら、自動再生。... したいのだが 何故か autoplay がうまくいかない。
+    
+    autoplay の参考はここから
+    https://discuss.streamlit.io/t/how-to-play-an-audio-file-automatically-generated-using-text-to-speech-in-streamlit/33201
--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from openai import OpenAI
 import streamlit as st
-# from dotenv import load_dotenv
 import os
-
-# load_dotenv()
-
+import time
+import base64
 from audio_recorder_streamlit import audio_recorder
 
 # ページ遷移を管理するための関数
@@ -15,6 +13,7 @@ def go_to_page(page_name):
 def current_page_is(page_name):
     return st.session_state.current_page == page_name
 
+# ChatBotクラスの定義
 class ChatBot:
     def __init__(self, client, model_name, system_message, max_input_history=2):
         self.client = client
@@ -60,47 +59,57 @@ def chat_page():
     # 利用可能なモデルと音声のリスト
     MODEL_NAMES = ["gpt-3.5-turbo-1106", "gpt-4-1106-preview"]
     VOICES = ["alloy", "echo", "fable", "onyx", "nova", "shimmer"]
+    # 音声ファイルのパス
+    input_audio_file = "recorded_audio.wav"
+    output_audio_file = "speech.mp3"
 
     # Streamlitのselectboxを使用してユーザーが選択できるようにする
-    selected_model_name = st.selectbox("モデルを選択してください", MODEL_NAMES)
-    selected_voice = st.selectbox("音声を選択してください", VOICES)
+    selected_model_name = st.selectbox("モデルを選択", MODEL_NAMES)
+    selected_voice = st.selectbox("音声を選択", VOICES)
     if "user_input" not in st.session_state:
-        # st.session_state.user_input = "フレンドリーなやり取りを行って下さい"
-        # st.session_state.user_input = "入力を「やさしい日本語」に変換して下さい。難しい単語、漢語は簡単なやまと言葉に変えて下さい"
-        st.session_state.user_input = "簡単な英文のシャドウイングを行います。ユーザーが「はじめて」と言ったら、簡単な英文を出してユーザーの言葉を聞いて下さい。ユーザーの言葉を聞いたらフィードバックを行い、次のシャドウイングの英文を出して下さい。「おわって」でシャドウイングを終わります"
+        st.session_state.user_input = "フレンドリーなやり取りを行って下さい"
+        # st.session_state.user_input = "簡単な英文のシャドウイングを行います。ユーザーが「はじめて」と言ったら、簡単な英文を出してユーザーの言葉を聞いて下さい。ユーザーの言葉を聞いたらフィードバックを行い、次のシャドウイングの英文を出して下さい。「おわって」でシャドウイングを終わります"
+        # st.session_state.user_input = "入力を「やさしい日本語」に変換して下さい。難しい単語や漢語は簡単なやまと言葉に変えて下さい"
 
-    user_input = st.text_input("system promptを設定してください", value=st.session_state.user_input)
+    user_input = st.text_input("システムプロンプトを設定", value=st.session_state.user_input)
 
     if user_input:
         chatbot = initialize_chatbot(client, user_input, selected_model_name)
-        audio_bytes = audio_recorder(text = "クリックして話し始めてください", pause_threshold=1.2)
+        audio_bytes = audio_recorder(text = "クリックして話す", 
+                                     icon_name="microphone-lines",
+                                     neutral_color="#6aa36f", pause_threshold=1.2)
         if audio_bytes:
-            st.audio(audio_bytes, format="audio/wav")
-            write_audio_file("recorded_audio.wav", audio_bytes)
+            # st.audio(audio_bytes, format="audio/wav")
+            write_audio_file(input_audio_file, audio_bytes)
 
             transcript = client.audio.transcriptions.create(
                 model="whisper-1",
-                file=open("recorded_audio.wav", "rb"),
+                file=open(input_audio_file, "rb"),
             )
-            st.text(transcript.text)
+            st.write("入力:", transcript.text)
 
+            # AIの応答を取得
             response_chatgpt = chatbot.get_ai_response(transcript.text)
+            # 得られた応答を音声に変換
             response = client.audio.speech.create(
                 model="tts-1", voice=selected_voice, input=response_chatgpt
             )
+            # 応答を音声ファイルとして保存
+            response.stream_to_file(output_audio_file)  
 
-            # try:
-            response.stream_to_file("speech.mp3")  
-            #except Warning as w:
-            #    print(f"Error: {w}")
+            # 応答を表示
+            st.write("応答:", response_chatgpt)
 
-            st.audio(read_audio_file("speech.mp3"), format="audio/mp3")
+            # 応答音声を再生
+            data = read_audio_file(output_audio_file)
+            st.audio(data, format="audio/mp3")
+            # autoplay_audio(read_audio_file(output_audio_file))
 
-            transcript = client.audio.transcriptions.create(
-                model="whisper-1",
-                file=open("speech.mp3", "rb"),
-            )
-            st.write(transcript.text)
+            # transcript = client.audio.transcriptions.create(
+            #     model="whisper-1",
+            #     file=open(output_audio_file, "rb"),
+            # )
+            # st.write(transcript.text)
 
 
 def initialize_chatbot(client, user_input, selected_model_name):
@@ -114,6 +123,7 @@ def initialize_chatbot(client, user_input, selected_model_name):
         st.session_state.user_input = user_input
     return st.session_state.chatbot
 
+# @st.cache(allow_output_mutation=True)
 def read_audio_file(file_path):
     with open(file_path, "rb") as audio_file:
         return audio_file.read()
@@ -121,6 +131,28 @@ def read_audio_file(file_path):
 def write_audio_file(file_path, audio_bytes):
     with open(file_path, "wb") as audio_file:
         audio_file.write(audio_bytes)
+        audio_file.flush()
+        os.fsync(audio_file.fileno())
+
+# @st.cache_data(allows_output_mutation=True)
+def autoplay_audio(data):
+    if data:
+        b64 = base64.b64encode(data).decode()
+        print(f"b64[0:10]: {b64[0:20]},{time.time()}")
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        placeholder = st.empty()
+        placeholder.markdown(
+            md,
+            unsafe_allow_html=True,
+        )   
+        #st.markdown(
+        #    md,
+        #    unsafe_allow_html=True,
+        #)
 
 # ページ遷移のロジック
 if 'current_page' not in st.session_state:
